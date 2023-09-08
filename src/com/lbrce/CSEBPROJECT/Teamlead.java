24f6fcecf4b5d0a58ccde798c4defbb87815719c
Admin <Admin@DESKTOP-TI6JGS8>
2023-09-08 15:02:46 +0530

    srikanth commit

--- src/com/lbrce/CSEBPROJECT/Teamlead.java
+++ src/com/lbrce/CSEBPROJECT/Teamlead.java
@@ -3,8 +3,12 @@ package com.lbrce.CSEBPROJECT;
 public class Teamlead {
 	public static void main(String ards[]) {
 		System.out.println(fun());
+		System.out.println(sriks());
 	}
 	public static String fun() {
 		return "hello this is teamlead class";
+	}
+	public static String sriks() {
+		return "Hey Kumar this is Srikanth";
 	}
 }

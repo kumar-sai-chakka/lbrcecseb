51b470ae5782d1c60f718afe7dbda670e4336a92
AdmiN <AdmiN@ITWS60>
2023-09-08 15:00:49 +0530

    Muralikrishna-Commit

--- src/com/lbrce/CSEBPROJECT/Teamlead.java
+++ src/com/lbrce/CSEBPROJECT/Teamlead.java
@@ -3,8 +3,13 @@ package com.lbrce.CSEBPROJECT;
 public class Teamlead {
 	public static void main(String ards[]) {
 		System.out.println(fun());
+		CSEC7();
 	}
 	public static String fun() {
 		return "hello this is teamlead class";
+	}
+	public static void CSEC7()
+	{
+		System.out.println("Hello this is MuraliKrishna");
 	}
 }

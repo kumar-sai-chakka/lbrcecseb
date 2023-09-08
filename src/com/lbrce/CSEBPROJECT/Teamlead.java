85e6f565b424f0a286a17ba97010e78fee0fe50b
Admin <Admin@DESKTOP-UK9TSQ1>
2023-09-08 14:15:56 +0530

    first commit

---
+++ src/com/lbrce/CSEBPROJECT/Teamlead.java
@@ -0,0 +1,10 @@
+package com.lbrce.CSEBPROJECT;
+
+public class Teamlead {
+	public static void main(String ards[]) {
+		System.out.println(fun());
+	}
+	public static String fun() {
+		return "hello this is teamlead class";
+	}
+}
